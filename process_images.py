8a820565114b00c0f668df9eec5dca09f9f884f9
Goblin <145717103+ImGoblin@users.noreply.github.com>
2024-06-21 13:20:49 +0300

    Update process_images.py

--- process_images.py
+++ process_images.py
@@ -1,12 +1,11 @@
 import os
-import subprocess
 from PIL import Image
 
-def process_image(image_path, output_path, size):
+def process_image(image_path, size):
     try:
         with Image.open(image_path) as img:
             img = img.resize(size, Image.LANCZOS)
-            img.save(output_path)
+            img.save(image_path)
         print(f"Обработано изображение {image_path}")
     except Exception as e:
         print(f"Не удалось обработать изображение {image_path}: {e}")
@@ -16,23 +15,20 @@ def process_directory(directory):
         for file in files:
             if file.lower().endswith(('png', 'jpg', 'jpeg', 'gif', 'bmp')):
                 image_path = os.path.join(root, file)
-                # Определяем размер для горизонтальных и вертикальных изображений
                 try:
                     with Image.open(image_path) as img:
+                        # Определяем размеры для горизонтальных и вертикальных изображений
                         if img.width > img.height:
                             size = (760, 590)
                         else:
                             size = (442, 590)
                     
-                    # Создаём новый путь для сохранения изменённого изображения
-                    output_path = os.path.join(root, f"resized_{file}")
-                    process_image(image_path, output_path, size)
-                    
-                    # Заменяем исходное изображение изменённым
-                    os.remove(image_path)
-                    os.rename(output_path, image_path)
+                    # Обрабатываем изображение
+                    process_image(image_path, size)
                     
                 except Exception as e:
                     print(f"Не удалось обработать изображение {image_path}: {e}")
 
-process_directory("static/images/")
+if __name__ == "__main__":
+    directory = "static/images/"
+    process_directory(directory)
